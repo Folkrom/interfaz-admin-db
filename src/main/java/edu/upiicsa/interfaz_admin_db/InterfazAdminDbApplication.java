4799c75c3a0956226304fbc341650d4b06b1afdc
Zabdiel Reyes <zabreor@gmail.com>
2024-04-22 20:22:50 -0600

    First commit

---
+++ src/main/java/edu/upiicsa/interfaz_admin_db/InterfazAdminDbApplication.java
@@ -0,0 +1,13 @@
+package edu.upiicsa.interfaz_admin_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfazAdminDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterfazAdminDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/upiicsa/interfaz_admin_db/InterfazAdminDbApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.upiicsa.interfaz_admin_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterfazAdminDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
